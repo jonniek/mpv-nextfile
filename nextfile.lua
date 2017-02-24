e0bfe6859c279f4d953f16a336e5e4c448f579f6
donmaiq <kayhty@windowslive.com>
2017-02-24 23:20:10 +0200

    removed not working parts

--- nextfile.lua
+++ nextfile.lua
@@ -1,5 +1,4 @@
 local utils = require 'mp.utils'
-local msg = require 'mp.msg'
 local settings = {
 
     --filetypes,{'mp4','mkv'} for specific or {''} for all filetypes
@@ -12,20 +11,6 @@ local settings = {
     --at end of directory jump to start and vice versa
     allow_looping = true,
 
-    --load next file automatically default value
-    --recommended to keep as false and cycle with toggle or set with a script message
-    --KEY script-message loadnextautomatically [true|false]
-    --KEY script-binding toggleauto
-    load_next_automatically = false,
-
-    accepted_eof_reasons = {
-        ['eof']=true,     --The file has ended. This can (but doesn't have to) include incomplete files or broken network connections under circumstances.
-        ['stop']=true,    --Playback was ended by a command.
-        ['quit']=false,    --Playback was ended by sending the quit command.
-        ['error']=true,   --An error happened. In this case, an error field is present with the error string.
-        ['redirect']=true,--Happens with playlists and similar. Details see MPV_END_FILE_REASON_REDIRECT in the C API.
-        ['unknown']=true, --Unknown. Normally doesn't happen, unless the Lua API is out of sync with the C API.
-    }
 }
 --check os
 if settings.linux_over_windows==nil then
@@ -37,23 +22,6 @@ if settings.linux_over_windows==nil then
   end
 end
 
-local lock = true --to avoid infinite loops
-function on_loaded()
-    plen = mp.get_property_number('playlist-count')
-    path = utils.join_path(mp.get_property('working-directory'), mp.get_property('path'))
-    file = mp.get_property("filename")
-    dir = utils.split_path(path)
-    lock = true
-end
-
-function on_close(reason)
-    if settings.accepted_eof_reasons[reason.reason] and settings.load_next_automatically and lock then
-        msg.info("Loading next file in directory")
-        mp.command("playlist-clear")
-        nexthandler()
-    end
-end
-
 function nexthandler()
     movetofile(true)
 end
@@ -62,26 +30,19 @@ function prevhandler()
     movetofile(false)
 end
 
-function toggleauto()
-    if not settings.load_next_automatically then
-        settings.load_next_automatically = true
-        if mp.get_property_number('playlist-count', 0) > 1 then
-            mp.osd_message("Playlist will be purged when loading new file")
-        else
-            mp.osd_message("Loading next when file ends")
-        end
-    else
-        settings.load_next_automatically = false
-        mp.osd_message("Not loading next when file ends")
-    end
-end
-
 function escapepath(dir, escapechar)
   return string.gsub(dir, escapechar, '\\'..escapechar)
 end
 
 function movetofile(forward)
-    lock = false
+    local pwd = mp.get_property('working-directory')
+    local relpath = mp.get_property('path')
+    if not pwd or not relpath then return end
+
+    local path = utils.join_path(pwd, relpath)
+    local file = mp.get_property("filename")
+    local dir = utils.split_path(path)
+
     local search = ' '
     for w in pairs(settings.filetypes) do
         if settings.linux_over_windows then
@@ -133,23 +94,9 @@ function movetofile(forward)
         end
         popen:close()
     else
-        msg.error("could not scan for files: "..(err or ""))
+        mp.msg.error("could not scan for files: "..(err or ""))
     end
 end
 
---read settings from a script message
-function loadnext(msg, value)
-  if msg == "next" then nexthandler() ; return end
-  if msg == "previous" then prevhandler() ; return end
-  if msg == "auto" then
-    if value == "toggle" then toggleauto() ; return end
-    toggleauto(value:lower() == 'true' )
-  end
-end
-
-mp.register_script_message("nextfile", loadnext)
-mp.add_key_binding('SHIFT+PGDWN', 'nextfile', nexthandler)
-mp.add_key_binding('SHIFT+PGUP', 'previousfile', prevhandler)
-mp.add_key_binding('CTRL+N', 'autonextfiletoggle', toggleauto)
-mp.register_event('file-loaded', on_loaded)
-mp.register_event('end-file', on_close)
+mp.add_key_binding('shift+RIGHT', 'nextfile', nexthandler)
+mp.add_key_binding('shift+LEFT', 'previousfile', prevhandler)
